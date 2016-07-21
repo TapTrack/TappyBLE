57374b150bf932b7bd920a5b4afce44dc636d889
Luke Van Oort <johnvanoort@gmail.com>
2016-07-21 16:10:36 -0400

    Adding all uri codes

--- tappy-constants/src/main/java/com/taptrack/tcmptappy/tappy/constants/NdefUriCodes.java
+++ tappy-constants/src/main/java/com/taptrack/tcmptappy/tappy/constants/NdefUriCodes.java
@@ -24,5 +24,33 @@ public interface NdefUriCodes {
     public static final byte URICODE_HTTPS = 0x04;
     public static final byte URICODE_TEL = 0x05;
     public static final byte URICODE_MAILTO = 0x06;
-
+    public static final byte URICODE_FTP_ANON = 0x07;
+    public static final byte URICODE_FTP_FTP = 0x08; // ftp://ftp
+    public static final byte URICODE_FTPS = 0x09;
+    public static final byte URICODE_SFTP = 0x0A;
+    public static final byte URICODE_SMB = 0x0B;
+    public static final byte URICODE_NFS = 0x0C;
+    public static final byte URICODE_FTP = 0x0D;
+    public static final byte URICODE_DAV = 0x0E;
+    public static final byte URICODE_NEWS = 0x0F;
+    public static final byte URICODE_TELNET = 0x10;
+    public static final byte URICODE_IMAP = 0x11;
+    public static final byte URICODE_RTSP = 0x12;
+    public static final byte URICODE_URN = 0x13;
+    public static final byte URICODE_POP = 0x14;
+    public static final byte URICODE_SIP = 0x15;
+    public static final byte URICODE_SIPS = 0x16;
+    public static final byte URICODE_TFTP = 0x17;
+    public static final byte URICODE_BTSPP = 0x18;
+    public static final byte URICODE_BTL2CAP = 0x19;
+    public static final byte URICODE_BTGOEP = 0x1A;
+    public static final byte URICODE_TCPOBEX = 0x1B;
+    public static final byte URICODE_IRDAOBEX = 0x1C;
+    public static final byte URICODE_FILE = 0x1E;
+    public static final byte URICODE_URN_EPC_ID = 0x1E;
+    public static final byte URICODE_URN_EPC_TAG = 0x1F;
+    public static final byte URICODE_URN_EPC_PAT = 0x20;
+    public static final byte URICODE_URN_EPC_RAW = 0x21;
+    public static final byte URICODE_URN_EPC = 0x22;
+    public static final byte URICODE_URN_NFC = 0x23;
 }
